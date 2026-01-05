c98862d09a8e343e5e2f5c41aa194de8fec93458
Brett Nielsen <brett.nielsen@tireguru.net>
2026-01-05 12:25:57 -0700

    Initial Commit

---
+++ jsonview/jsonviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonviewApp.swift
+//  jsonview
+//
+//  Created by Brett Nielsen on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct jsonviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
